0c54cc068707720d1a0584c79b1efda6b95d2863
GamerCleanVic <jonassd2000@gmail.com>
2023-11-01 20:19:32 -0500

    arraySizeOf

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    $ingredientes = ['Açucar', 'Farinha de trigo', 'ovo',
+        'leite', 'fermento em pó'
+    ];
+
+    for($i = 0; $i < sizeof($ingredientes); $i++){
+        echo "-> ".$ingredientes[$i]."<br>";
+    }
+?>
