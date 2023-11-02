1beb08d1d08149bc221c745c67133a31ac810118
GamerCleanVic <jonassd2000@gmail.com>
2023-11-02 01:08:06 -0500

    Dias da semana

--- index.php
+++ index.php
@@ -1,19 +1,45 @@
 <?php
-    $userOne = [
-        'name' => 'Jhon',
-        'age' => 20
-    ];
+    function semana(){
+        echo "
+            <hr />Domingo: 1<hr />
+            Segunda: 2<hr />
+            Terça: 3<hr />
+            Quarta: 4<hr />
+            Quinta: 5<hr />
+            Sexta: 6<hr />
+            Sábado: 7<hr />
+        ";
+        $dia = [
+            "Domingo", "Segunda", "Terça", "Quarta", "Quinta",
+            "Sexta", "Sábado"
+        ];
 
-    $userTwo = [
-        'name' => 'Jhon',
-        'age' => '20'
-    ];
-
-    if($userOne === $userTwo){
-        var_dump($userOne === $userTwo);
-        echo "<hr />E verdadeiro!";
-    }else{
-        var_dump($userOne === $userTwo);
-        echo "<hr />E falso!";
+        $num = readline("Digite um nº da lista acima: ");
+        switch($num){
+            case 1:
+                echo "<br />O dia $num º da semana é ".$dia[0];
+                break;
+            case 2:
+                echo "<br />O dia $num º da semana é ".$dia[1];
+                break;
+            case 3:
+                echo "<br />O dia $num º da semana é ".$dia[2];
+                break;
+            case 4:
+                echo "<br />O dia $num º da semana é".$dia[3];
+                break;
+            case 5:
+                echo "<br />O dia $num º da semana é ".$dia[4];
+                break;
+            case 6:
+                echo "<br />O dia $num º da semana é ".$dia[5];
+                break;
+            case 7:
+                echo "<br />O dia $num º da semana é ".$dia[6];
+                break;
+            default:
+                echo "<br />Digite um número da semana que esteja na lista acima.";
+        }
     }
+    semana();
 ?>
