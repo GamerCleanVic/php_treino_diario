c1edec1a456ae1a27fc9c1c27eaea367e7c91534
GamerCleanVic <jonassd2000@gmail.com>
2023-11-04 20:18:39 -0500

    getSet2

--- index.php
+++ index.php
@@ -1,9 +1,41 @@
 <?php
-    class Operacoes{
-          public function calc($x, $y){
-               return $x + $y;
-          }
-    }
+     class Post{
+          private int $id;
+          private int $likes;
+          private array $comments = [];
+          private string $author;
 
-$operaFora = new Operacoes();
-echo $operaFora->calc(3, 4);
+          public function setAuthor($nome){
+               $this->author = ucfirst($nome);
+          }
+          public function getAuthor(){
+               return $this->author;
+          }
+
+          public function setLikes($liked){
+               $this->likes = $liked;
+          }
+          public function getLikes(){
+               return $this->likes;
+          }
+
+          public function setId($fnId){
+               $this->id = $fnId;
+          }
+          public function getId(){
+               return $this->id;
+          }
+     }
+
+     $post1 = new Post();
+     $post1->setAuthor('fulano');
+     $post1->setId(1);
+     $post1->setLikes(12);
+
+     $post2 = new Post();
+     $post2->setAuthor('Cicrano');
+     $post2->setId(2);
+     $post2->setLikes(7);
+
+     echo "Id".$post1->getId()." - Author - ".$post1->getAuthor()." - Comments - ".$post1->getLikes()."<br />";
+     echo "Id".$post2->getId()." - Author - ".$post2->getAuthor()." - Comments - ".$post2->getLikes()."<br />";
